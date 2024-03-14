aa264eddf30d252ae36c2b9b2a6140ba3ed4c3e4
lilmena24 <za210110151@zapopan.tecmm.edu.mx>
2024-03-13 19:54:54 -0600

    Carreras.Page

--- Future-Path-Finder/src/CarrerasPage/CarrerasPage.jsx
+++ Future-Path-Finder/src/CarrerasPage/CarrerasPage.jsx
@@ -1,23 +1,69 @@
 import React from 'react';
-import './CarrerasPage.css';
+import './CarrerasPage.css'; // Importa los estilos CSS
 
 function CarrerasPage() {
-    return (
-        <div>
-            <header>
-                <h1>Future Path Finder</h1>
-            </header>
-            <nav>
-                <ul>
-                    <li><a className="linkBarra" href="#">Carreras</a></li>
-                </ul>
-                <ul>
-                    <li><a className="linkBarra" href="#">Test</a></li>
-                </ul>
-            </nav>
-            <section className="spacer"></section>
+  return (
+    <div>
+      <header>
+        <h1>Future Path Finder</h1>
+      </header>
+      <nav>
+        <ul>
+          <li><a className="linkBarra" href="#">Inicio</a></li>
+        </ul>
+        <ul>
+          <li><a className="linkBarra" href="#">Test</a></li>
+        </ul>
+      </nav>
+      <section>
+        <div className="area-box">
+          <div className="area-title">Science</div>
+          <div className="area-description">
+            <ul>
+              <li>Biology</li>
+              <li>Description: Study of living organisms.</li>
+              <li>Duration: 4 years</li>
+            </ul>
+          </div>
         </div>
-    );
+
+        <div className="area-box">
+          <div className="area-title">Technology</div>
+          <div className="area-description">
+            <ul>
+              <li>Computer Science</li>
+              <li>Description: Study of algorithms, programming languages, and computer hardware.</li>
+              <li>Duration: 4 years</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="area-box">
+          <div className="area-title">Engineering</div>
+          <div className="area-description">
+            <ul>
+              <li>Mechanical Engineering</li>
+              <li>Description: Study of mechanical systems.</li>
+              <li>Duration: 4 years</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="area-box">
+          <div className="area-title">Math</div>
+          <div className="area-description">
+            <ul>
+              <li>Math</li>
+              <li>Description: Study of numbers, quantities, and shapes.</li>
+              <li>Duration: 4 years</li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <footer>
+      </footer>
+    </div>
+  );
 }
 
 export default CarrerasPage;
