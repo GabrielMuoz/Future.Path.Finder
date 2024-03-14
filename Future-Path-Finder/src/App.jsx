fbaccb0f827b4afb5e3534cf9f5f30c4fa543425
GabrielMuoz <gabriel.mu.bri@gmail.com>
2024-03-13 20:01:13 -0600

    a

--- Future-Path-Finder/src/App.jsx
+++ Future-Path-Finder/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import HomePage from './HomePage/HomePage';
+import CarrerasPage from './CarrerasPage/CarrerasPage'
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <CarrerasPage/>
     </div>
   );
 }
