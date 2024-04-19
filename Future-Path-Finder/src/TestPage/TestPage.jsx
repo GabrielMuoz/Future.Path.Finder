475bcf045154ba6f1444f199f5360ca8450260a0
GabrielMuoz <gabriel.mu.bri@gmail.com>
2024-04-19 15:37:17 -0600

    a

--- Future-Path-Finder/src/TestPage/TestPage.jsx
+++ Future-Path-Finder/src/TestPage/TestPage.jsx
@@ -14,8 +14,11 @@ const links=[
     },
 ];
 
+
 function TestPage() {
+  const history = useHistory();
   const [preguntas, setPreguntas] = useState([]);
+  const [respuestas, setRespuestas] = useState([]);
 
   useEffect(() => {
     const fetchPreguntas = async () => {
@@ -23,6 +26,7 @@ function TestPage() {
         const response = await axios.get('http://localhost:3000/api/test');
         console.log('Datos devueltos de la API:', response.data); 
         setPreguntas(response.data);
+        setRespuestas(Array(response.data.length).fill(''));
       } catch (error) {
         console.error('Error al obtener las preguntas:', error);
       }
@@ -30,6 +34,38 @@ function TestPage() {
   
     fetchPreguntas();
   }, []);
+
+  const handleRespuestaChange = (index, value) => {
+    const nuevasRespuestas = [...respuestas];
+    nuevasRespuestas[index] = value;
+    setRespuestas(nuevasRespuestas);
+  };
+
+  const calcularCalificaciones = () => {
+    const calificaciones = {
+      ciencia: 0,
+      matematicas: 0,
+      ingenieria: 0,
+      tecnologia: 0
+    };
+
+    preguntas.forEach((pregunta, index) => {
+      const respuestaSeleccionada = respuestas[index];
+      const respuestaCorrecta = pregunta.opciones.findIndex(opcion => opcion.correcta);
+
+      if (!respuestaSeleccionada || respuestaSeleccionada !== pregunta.opciones[respuestaCorrecta].texto) {
+        calificaciones[pregunta.area.toLowerCase()]++;
+      }
+    });
+
+    console.log('Calificaciones:', calificaciones);
+
+    // Redirige a la página de calificaciones con las calificaciones como parámetro de consulta
+    history.push({
+      pathname: '/calificaciones',
+      state: { calificaciones: calificaciones }
+    });
+  };
 
   return (
     <div>
@@ -58,7 +94,7 @@ function TestPage() {
           </div>
         )) : <p>No hay preguntas disponibles</p>}
         <div className="centrarBoton">
-          <button className="botonTerminar">Terminar</button>
+         <button className="botonTerminar" onClick={calcularCalificaciones}>Terminar</button>
         </div>
       </div>
     </div>
