c7d8f6f623b8a845a8ff20025d296d45204467b5
GabrielMuoz <gabriel.mu.bri@gmail.com>
2024-05-05 21:02:57 -0600

    a

--- Future-Path-Finder/src/TestPage/TestPage.jsx
+++ Future-Path-Finder/src/TestPage/TestPage.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import './TestPage.css'; 
+import './TestPage.css';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import jsPDF from 'jspdf';
 
-const links=[
+
+const links = [
     {
         name: "Regresar al inicio",
         href: "/"
@@ -14,149 +16,192 @@ const links=[
     },
 ];
 
+
+const generatePDF = (contadores, carreras) => {
+  const doc = new jsPDF();
+  doc.setFontSize(18);
+  doc.text('Resultados del Test', 10, 10);
+
+  // Agregar resultados del test al PDF
+  let yPos = 30;
+  Object.keys(contadores).forEach((area, index) => {
+      const porcentaje = contadores[area];
+      doc.setFontSize(12);
+      doc.text(`${area}: ${porcentaje}%`, 10, yPos);
+      yPos += 10;
+      doc.rect(10, yPos, porcentaje * 1.5, 5, 'F');
+      yPos += 10;
+  });
+
+  // Agregar carreras recomendadas al PDF
+  yPos += 10;
+  doc.setFontSize(16);
+  doc.text('Carreras Recomendadas:', 10, yPos);
+  yPos += 10;
+  carreras.forEach((carrera, index) => {
+      doc.text(`${index + 1}. ${carrera.nombre} - ${carrera.area}`, 15, yPos);
+      yPos += 10;
+  });
+
+  return doc.output('blob'); // Devolver el objeto Blob del PDF
+};
+
+
 function TestPage() {
-  const [preguntas, setPreguntas] = useState([]);
-  const [respuestas, setRespuestas] = useState({});
-  const [contadores, setContadores] = useState({});
-  const [showPopup, setShowPopup] = useState(false);
-  const [carreras, setCarreras] = useState([]);
-  const [carrerasAleatorias, setCarrerasAleatorias] = useState([]);
+    const [preguntas, setPreguntas] = useState([]);
+    const [respuestas, setRespuestas] = useState({});
+    const [contadores, setContadores] = useState({});
+    const [showPopup, setShowPopup] = useState(false);
+    const [carreras, setCarreras] = useState([]);
 
-  const obtenerCarreras = async () => {
-    try {
-      const response = await axios.get('http://localhost:3000/api/carreras');
-      setCarreras(response.data);
-    } catch (error) {
-      console.error('Error al obtener las carreras:', error);
-    }
-  };
-
-  const seleccionarCarrerasAleatorias = () => {
-    const carrerasSeleccionadas = [];
-    const carrerasDisponibles = [...carreras];
-    for (let i = 0; i < 3; i++) {
-      const indiceAleatorio = Math.floor(Math.random() * carrerasDisponibles.length);
-      const carreraAleatoria = carrerasDisponibles.splice(indiceAleatorio, 1)[0];
-      carrerasSeleccionadas.push(carreraAleatoria);
-    }
-    setCarrerasAleatorias(carrerasSeleccionadas);
-    setShowPopup(true);
-  };
-
-  useEffect(() => {
-    obtenerCarreras();
-  }, []);
-
-  useEffect(() => {
-    const fetchPreguntas = async () => {
-      try {
-        const response = await axios.get('http://localhost:3000/api/test');
-        const areas = response.data.reduce((acc, pregunta) => {
-          acc[pregunta.area] = acc[pregunta.area] || [];
-          acc[pregunta.area].push(pregunta);
-          return acc;
-        }, {});
-        const initialContadores = Object.keys(areas).reduce((acc, area) => {
-          acc[area] = 0;
-          return acc;
-        }, {});
-        setPreguntas(areas);
-        setContadores(initialContadores);
-      } catch (error) {
-        console.error('Error al obtener las preguntas:', error);
-      }
-    };
-    fetchPreguntas();
-  }, []);
-
-  const handleRespuesta = (area, indexPregunta, opcionSeleccionada) => {
-    setRespuestas(prevRespuestas => ({
-      ...prevRespuestas,
-      [`${area}-${indexPregunta}`]: opcionSeleccionada
-    }));
-  };
-
-  const calcularContadores = () => {
-    const newContadores = {};
-    Object.keys(preguntas).forEach(area => {
-      newContadores[area] = 0;
-      preguntas[area].forEach((pregunta, indexPregunta) => {
-        const opcionSeleccionada = respuestas[`${area}-${indexPregunta}`];
-        if (
-          opcionSeleccionada !== undefined &&
-          pregunta.opciones[opcionSeleccionada].correcta
-        ) {
-          newContadores[area]++;
+    useEffect(() => {
+        const storedAnswers = JSON.parse(localStorage.getItem('respuestas'));
+        if (storedAnswers) {
+            setRespuestas(storedAnswers);
         }
-      });
-      newContadores[area] = (newContadores[area] / 10) * 100;
-    });
-    setContadores(newContadores);
-    seleccionarCarrerasAleatorias(); 
-  };
+    }, []);
 
-  return (
-    <div>
-      <header>
-        <h1>Future Path Finder</h1>
-      </header>
-      <nav>
-        {links.map((y, index) => (
-          <Link key={index} to={y.href} className="linkBarra">{y.name}</Link>
-        ))}
-      </nav>
-      <h2 className="tituloTest">Test</h2>
-      <p className="cajaAdvertencia">recordatorio</p>
+    useEffect(() => {
+        localStorage.setItem('respuestas', JSON.stringify(respuestas));
+    }, [respuestas]);
 
-      <div className="recuadroPreguntas">
-        {Object.keys(preguntas).map((area, index) => (
-          <div key={index}>
-            {preguntas[area].map((pregunta, indexPregunta) => (
-              <div className="container" key={pregunta._id}>
-                <section id={`p${indexPregunta}`}>
-                  <h3>{pregunta.pregunta}</h3>
-                  {pregunta.opciones.map((opcion, i) => (
-                    <label key={i}>
-                      <input 
-                        type="radio" 
-                        value={opcion.texto} 
-                        name={`p${indexPregunta}`} 
-                        onChange={() => handleRespuesta(area, indexPregunta, i)}
-                      /> {opcion.texto} {opcion.correcta ? '(Correcta)' : ''}
-                    </label>
-                  ))}
-                </section>
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
-      <div className="centrarBoton">
-        <button className="botonTerminar" onClick={calcularContadores}>Terminar</button>
-      </div>
-      {showPopup && (
-        <div className="popup">
-          <span className="close" onClick={() => setShowPopup(false)}>&times;</span>
-          <h2>Resultados</h2>
-          <div className="Resultados">
-            {Object.keys(contadores).map((area, index) => (
-              <div key={index}>
-                <p>{area}: {contadores[area]}%</p>
-                <div className="barraDeCarga">
-                  <div className="progreso" style={{ width: `${contadores[area]}%` }}></div>
+    useEffect(() => {
+      const fetchData = async () => {
+          try {
+              const [preguntasResponse, carrerasResponse] = await Promise.all([
+                  axios.get('http://localhost:3000/api/test'),
+                  axios.get('http://localhost:3000/api/carreras')
+              ]);
+  
+              const areas = preguntasResponse.data.reduce((acc, pregunta) => {
+                  acc[pregunta.area] = acc[pregunta.area] || [];
+                  acc[pregunta.area].push(pregunta);
+                  return acc;
+              }, {});
+  
+              const initialContadores = Object.keys(areas).reduce((acc, area) => {
+                  acc[area] = 0;
+                  return acc;
+              }, {});
+  
+              setPreguntas(areas);
+              setContadores(initialContadores);
+              setCarreras(carrerasResponse.data.slice(0, 3)); // Mostrar solo las primeras tres carreras
+          } catch (error) {
+              console.error('Error al obtener los datos:', error);
+          }
+      };
+      fetchData();
+  }, []);
+
+    const handleRespuesta = (area, indexPregunta, opcionSeleccionada) => {
+        setRespuestas(prevRespuestas => ({
+            ...prevRespuestas,
+            [`${area}-${indexPregunta}`]: opcionSeleccionada
+        }));
+    };
+
+    const limpiarRespuestas = () => {
+        setRespuestas({});
+    };
+
+    const calcularContadores = () => {
+        const newContadores = {};
+        Object.keys(preguntas).forEach(area => {
+            newContadores[area] = 0;
+            preguntas[area].forEach((pregunta, indexPregunta) => {
+                const opcionSeleccionada = respuestas[`${area}-${indexPregunta}`];
+                if (
+                    opcionSeleccionada !== undefined &&
+                    pregunta.opciones[opcionSeleccionada].correcta
+                ) {
+                    newContadores[area]++;
+                }
+            });
+            newContadores[area] = (newContadores[area] / 10) * 100;
+        });
+
+        setContadores(newContadores);
+        setShowPopup(true);
+    };
+
+    return (
+        <div>
+            <header>
+                <h1>Future Path Finder</h1>
+            </header>
+            <nav>
+                {links.map((y, index) => (
+                    <Link key={index} to={y.href} className="linkBarra">{y.name}</Link>
+                ))}
+            </nav>
+            <h2 className="tituloTest">Test</h2>
+            <p className="cajaAdvertencia">recordatorio</p>
+
+            <div className="recuadroPreguntas">
+                {Object.keys(preguntas).map((area, index) => (
+                    <div key={index}>
+                        {preguntas[area].map((pregunta, indexPregunta) => (
+                            <div className="container" key={pregunta._id}>
+                                <section id={`p${indexPregunta}`}>
+                                    <h3>{pregunta.pregunta}</h3>
+                                    {pregunta.opciones.map((opcion, i) => (
+                                        <label key={i}>
+                                            <input
+                                                type="radio"
+                                                value={opcion.texto}
+                                                name={`p${indexPregunta}`}
+                                                onChange={() => handleRespuesta(area, indexPregunta, i)}
+                                                checked={respuestas[`${area}-${indexPregunta}`] === i}
+                                            /> {opcion.texto} {opcion.correcta ? '(Correcta)' : ''}
+                                        </label>
+                                    ))}
+                                </section>
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+            <div className="centrarBoton">
+                <button className="botonTerminar" onClick={() => { calcularContadores(); limpiarRespuestas(); }}>Terminar</button>
+            </div>
+            {showPopup && (
+                <div className="popup">
+                    <span className="close" onClick={() => setShowPopup(false)}>&times;</span>
+                    <h2>Resultados</h2>
+                    <div className="Resultados">
+                        {Object.keys(contadores).map((area, index) => (
+                            <div key={index}>
+                                <p>{area}: {contadores[area]}%</p>
+                                <div className="barraDeCarga">
+                                    <div className="progreso" style={{ width: `${contadores[area]}%` }}></div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                    <h3>Carreras Recomendadas:</h3>
+                    <ul>
+                      {carreras.map((carrera, index) => (
+                      <li key={index}>{carrera.nombre} - {carrera.area}</li>
+                       ))}
+                    </ul>
+                    <button className="botonDescargarPDF" onClick={() => { 
+                        const pdfBlob = generatePDF(contadores, carreras.map(carrera => ({ nombre: carrera.nombre, area: carrera.area })));
+                        // Descargar PDF
+                        const link = document.createElement('a');
+                        link.href = window.URL.createObjectURL(pdfBlob);
+                        link.download = 'resultados_test.pdf';
+                        link.click();
+                    }}>
+                        Descargar PDF
+                    </button>
+                    <button className="botonEnviarCorreo" onClick={solicitarCorreo}>
+                      Enviar por Correo Electrónico
+                    </button>
                 </div>
-              </div>
-            ))}
-            {carrerasAleatorias.map((carrera, index) => (
-              <div key={index}>
-                <p>{carrera.nombre}</p>
-              </div>
-            ))}
-          </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
 
 export default TestPage;
